d31dcbfdafc93f8bd76869b11c9d7f6bcd0b2980
Lieu Tuan vu <vub1906810@student.ctu.edu.vn>
2022-10-15 22:44:59 +0700

    15/10/2022 add pagination food could choose num on page, update cart, admin login, admin change password, admin get info, admin food management

--- route/food.js
+++ route/food.js
@@ -119,6 +119,37 @@ router.get('/get-foods/:pageCur', async (req, res) => {
         }))
 })
 
+router.get('/get-foods/:pageCur/:numOnPage', async (req, res) => {
+    await new Food()
+        .getAll()
+        .then((foods) => {
+            let numberToGet = req.params.numOnPage //số lượng món ăn trên 1 trang
+            let pageNum = Math.ceil(foods.length / numberToGet);
+            const pageCur = (req.params.pageCur > pageNum) ? pageNum : (req.params.pageCur < 1) ? 1 : req.params.pageCur
+            let foodss = []
+            let curIndex = (pageCur - 1) * numberToGet
+            let count = 0
+            while (foods[curIndex] != null && count < numberToGet) {
+                foodss.push(foods[curIndex])
+                curIndex++
+                count++
+            }
+            return res.status(200).json({
+                success: true,
+                countOnPage: foodss.length,
+                pageCur,
+                pageNum,
+                foods: foodss,
+            });
+        })
+        .catch((err) => setImmediate(() => {
+            return res.status(400).json({
+                success: false,
+                message: 'Quý khách vui lòng thử lại sau'
+            });
+        }))
+})
+
 router.get('/find-foods', async (req, res) => {
     let foods = [];
     let { name, type, prices, ration, review } = req.query
