4d8312884bac41c4ff707f6eceddcd5a24470d95
Lieu Tuan vu <vub1906810@student.ctu.edu.vn>
2022-10-26 22:16:32 +0700

    26/10/2022 party order api (client), create order note, post to vnpay

--- route/order.js
+++ route/order.js
@@ -277,10 +277,10 @@ router.get('/pay-for-food-order/:orderid', verifyToken, async (req, res) => {
 
 })
 
-router.post('/pay-for-food-order', verifyToken, async (req, res) => {
+router.post('/pay-for-food-order', async (req, res) => {
     const { orderid } = req.body
-    const decoded = jwt.verify(req.header('Authorization'), process.env.ACCESS_TOKEN_SECRET)
-    const customerid = decoded.CustomerId
+    // const decoded = jwt.verify(req.header('Authorization'), process.env.ACCESS_TOKEN_SECRET)
+    const customerid = 'KH4'
     await new Order().verifyOrderWithCustomer(customerid, orderid)
         .then(async (result) => {
             if (result) {
